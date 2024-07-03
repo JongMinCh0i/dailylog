9db8a3a197b2bebcd02a453d986336c6b30482c5
JongMinCh0i <wonderland314592@gmail.com>
2024-07-03 20:04:44 +0900

    컨트롤러 생성 및 테스트

---
+++ src/test/java/com/dailylog/controller/PostControllerTest.java
@@ -0,0 +1,28 @@
+package com.dailylog.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/posts 요청이 오면 Hello World! 를 리턴한다.")
+    void test() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!"))
+                .andDo(print());
+    }
+}
